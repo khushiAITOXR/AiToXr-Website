e6fa8e764e95153de4a65dff63c5f413e1cb8a1a
khushiAITOXR <Khushi.savaran@aitoxr.com>
2024-10-09 09:42:57 +0000

    adds all pages

--- src/app/sections/WhatWedo.tsx
+++ src/app/sections/WhatWedo.tsx
@@ -3,13 +3,17 @@ import { FiChevronRight, FiChevronDown, FiChevronUp } from 'react-icons/fi'; // 
 import Image from 'next/image';
 
 const serviceItems = [
-  { id: 1, name: 'AI/ML & Data Science' },
-  { id: 2, name: 'AR/VR Applications Development' },
-  { id: 3, name: 'Mobile App Development' },
-  { id: 4, name: 'Web Application Development' },
-  { id: 5, name: 'Blockchain Application Development' },
-  { id: 6, name: 'Digital Marketing Services' },
-  { id: 7, name: 'E-Commerce Application Development' },
+  { id: 1, name: 'Data Science' },
+  { id: 2, name: 'AI/ML Development' },
+  { id: 3, name: 'AR/VR Applications Development' },
+  { id: 4, name: 'Mobile App Development' },
+  { id: 5, name: 'Web Application Development' },
+  { id: 6, name: 'Blockchain Application Development' },
+  { id: 7, name: 'Digital Marketing Services' },
+  { id: 8, name: 'E-Commerce Application Development' },
+  { id: 9, name: 'Staff Augmentation at AItoXR' },
+  { id: 10, name: 'Dedicated Teams at AItoXR' },
+  { id: 11, name: 'Software Outsourcing at AItoXR' },
 ];
 
 interface ServiceContent {
@@ -20,49 +24,77 @@ interface ServiceContent {
 
 const serviceContent: Record<number, ServiceContent> = {
   1: {
-    title: 'AI/ML & Data Science ',
+    title: 'Data Science ',
     description: `Artificial Intelligence (AI), Machine Learning (ML), and Data Science are transforming industries across the globe, and at  AItoXR we use them to transform raw data into actionable insights. Our AI & ML-powered solutions are designed to help businesses utilize data, gain actionable insights, and automate decision-making processes. With our expertise in predictive modeling, data mining, and machine learning algorithms, we develop progressive solutions that enhance operational efficiency, personalize customer experiences, and drive business innovation.
     At AItoXR, we ensure that your business stays ahead by using advanced data science techniques that enable smarter, data-driven decisions.
     `,
     technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
   },
   2: {
-    title: 'AR/VR Applications Development ',
+    title: 'AI/ML Development',
     description: `Augmented Reality (AR) and Virtual Reality (VR) are revolutionizing the way we interact with digital content. At AItoXR, we combine creativity with trending technology to deliver immersive AR and VR experiences that enhance engagement and open new business opportunities. From virtual training environments to interactive marketing campaigns, our AR and VR services transform the way businesses interact with their customers.
     With our AR/VR development services, businesses can create unforgettable experiences that captivate users and differentiate their brand in competitive markets.
     `,
     technologies: ['/python.svg', '/java.svg', '/julia.svg'],
   },
   3: {
-    title: 'Mobile App Development',
+    title: 'AR/VR Applications Development',
     description: `Mobile apps are at the core of today’s digital transformation. At AItoXR, we develop innovative mobile applications that are user-centric, scalable, and packed with powerful functionalities. Our mobile app development services cover everything from concept to deployment, ensuring that your app meets the highest standards of quality and performance. Whether it’s iOS or Android, AiToXr’s mobile app solutions are designed to enhance user experiences and drive business growth.
     Our expertise covers a broad spectrum of industries, ensuring your mobile app provides a seamless experience, fosters customer loyalty, and drives business growth.
     `,
     technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
   },
   4: {
-    title: 'Web Application Development ',
+    title: 'Mobile App Development',
     description: `At AItoXR, we design and develop web applications that are robust, secure, and scalable. Our web solutions are custom-built to fit your unique business needs, and the services include custom solutions, e-commerce platforms, and enterprise applications. Considering performance and user experience, we'll make sure that your web application operates with top-notch efficiency and delivers value to its users.
     Using the latest web development technologies, AItoXR creates web applications that are dynamic, responsive, and ready to grow with your business.
     `,
     technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
   },
   5: {
-    title: 'Blockchain based Application Development',
+    title: 'Web Application Development',
     description: `Blockchain is reshaping industries by enhancing transparency, security, and efficiency in transactions. AItoXR provides blockchain development services to help businesses adopt this transformative technology. We ensure secure, scalable, and innovative blockchain implementations for creating decentralized applications (dApps), smart contracts, and blockchain-based supply chain solutions.
     With AItoXR, you can use blockchain to streamline processes, reduce costs, and improve trust in your operations.
     `,
     technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
   },
   6: {
-    title: 'Digital Marketing Services',
+    title: 'Blockchain Application Development',
     description: `AiToXr’s digital marketing services help businesses reach their target audience and achieve their marketing goals that help businesses boost their online presence, attract leads, and grow revenue. From SEO and social media marketing to content creation and paid advertising, our strategies are data-driven and customized to meet your business goals.
     Our services in digital marketing focus on creating personalized experiences for your audience, maximizing engagement, and delivering measurable results that drive business success.
     `,
     technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
   },
   7: {
+    title: 'Digital Marketing Services',
+    description: `AItoXR specializes in building e-commerce platforms that are robust and deliver seamless online shopping experiences. Our expertise in e-commerce ensures that your online business is equipped to succeed in a competitive market.
+    With AItoXR's expertise, your e-commerce platform will be equipped to meet the demands of today’s digital consumers and scale effortlessly as your business grow.
+    `,
+    technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
+  },
+  8: {
     title: 'E-Commerce Application Development',
+    description: `AItoXR specializes in building e-commerce platforms that are robust and deliver seamless online shopping experiences. Our expertise in e-commerce ensures that your online business is equipped to succeed in a competitive market.
+    With AItoXR's expertise, your e-commerce platform will be equipped to meet the demands of today’s digital consumers and scale effortlessly as your business grow.
+    `,
+    technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
+  },
+  9: {
+    title: 'Staff Augmentation at AItoXR',
+    description: `AItoXR specializes in building e-commerce platforms that are robust and deliver seamless online shopping experiences. Our expertise in e-commerce ensures that your online business is equipped to succeed in a competitive market.
+    With AItoXR's expertise, your e-commerce platform will be equipped to meet the demands of today’s digital consumers and scale effortlessly as your business grow.
+    `,
+    technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
+  },
+  10: {
+    title: 'Dedicated Teams at AItoXR',
+    description: `AItoXR specializes in building e-commerce platforms that are robust and deliver seamless online shopping experiences. Our expertise in e-commerce ensures that your online business is equipped to succeed in a competitive market.
+    With AItoXR's expertise, your e-commerce platform will be equipped to meet the demands of today’s digital consumers and scale effortlessly as your business grow.
+    `,
+    technologies: ['/python.svg', '/r.svg', '/cpp.svg',  '/r.svg', '/cpp.svg'],
+  },
+  11: {
+    title: 'Software Outsourcing at AItoXR',
     description: `AItoXR specializes in building e-commerce platforms that are robust and deliver seamless online shopping experiences. Our expertise in e-commerce ensures that your online business is equipped to succeed in a competitive market.
     With AItoXR's expertise, your e-commerce platform will be equipped to meet the demands of today’s digital consumers and scale effortlessly as your business grow.
     `,
@@ -72,10 +104,17 @@ const serviceContent: Record<number, ServiceContent> = {
 
 const openRoute = (id: number) => {
   const route = 
-    id === 1 ? '/ai-ml' :
-    id === 2 ? '/ai-vr' :
-    id === 3 ? '/app-development' :
-    id === 4 ? '/web-development' :
+    id === 1 ? '/data-science' :
+    id === 2 ? '/ai-ml' :
+    id === 3 ? '/ai-vr' :
+    id === 4 ? '/app-development' :
+    id === 5 ? '/web-development' :
+    id === 6 ? '/blockchain-development' :
+    id === 7 ? '/digital-marketing' :
+    id === 8 ? '/ecommerce-development' :
+    id === 9 ? '/staff-augmentation' :
+    id === 10 ? '/dedicated-teams' :
+    id === 11 ? '/software-outsourcing' :
     '/coming-soon';
   window.open(route, '_blank');
 };
